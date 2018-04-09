84186bab44e7ba8ec9d03ee7d793b18f929e8871
Robert Brown <robertlb4@gmail.com>
2018-04-09 13:22:23 -0700

    first commit: add state object, createElement and createHTML functions as well as render function

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const state = {
+  Player: 1,
+  board: ['&nbsp;','&nbsp;','&nbsp;','&nbsp;','&nbsp;','&nbsp;','&nbsp;','&nbsp;','&nbsp;'],
+  Winner: undefined,
 };
+
+function createElement (cell, index) {
+  return `
+  <div class="cell" id="${index}">
+     <p>${cell}</p>
+  </div>
+  `;
+}
+
+
+function createHTML (array) {
+  let html = '';
+  array.forEach((cell, index) => {
+    if(index === 0) html += '<div class="row">';
+    if(index % 3 === 2) {
+      html += createElement(cell, index);
+      html += (index < 8 ) ? '</div> <div class="row">' : '</div>';
+    } else {
+      html += createElement(cell, index);
+    }
+  });
+  return html;
+}
 
 // State modification functions
 
 // Render functions
+function renderBoard() {
+  $('.board').html(createHTML(state.board));
+  console.log('render ran');
+}
 
 // Event Listeners
+
+$(renderBoard);
